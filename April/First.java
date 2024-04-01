2389395c5d152bc20fa021dbe7789ef608d29632
sikand34 <sikand1365.be21@chitkara.edu.in>
2024-04-01 10:33:42 +0530

    committed

--- April/First.java
+++ April/First.java
@@ -1,5 +1,33 @@
 public class First{
-    public static void main(String[] args) {
-        
+    class Solution {
+        public int lengthOfLastWord(String s) {
+            int l = s.length()-1;
+            boolean flag = false;
+            int a = 0;
+            while(true){
+                if(l>=0){
+                    if(s.charAt(l) == ' '){
+                    if(flag == true){
+                        return a;
+                        
+                    }
+                    else{
+                        l--;
+                    }
+                }
+                else{
+                    a++;
+                    l--;
+                    flag = true;
+                }
+                }
+                else{
+                    return a;
+                }
+                
+                
+            }
+            
+        }
     }
 }
