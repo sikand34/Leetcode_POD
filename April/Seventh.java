b5e282590b6ea21b42eff5f23c04d47f63666e3f
sikand34 <sikand1365.be21@chitkara.edu.in>
2024-04-07 15:25:17 +0530

    Seventh April Updated

---
+++ April/Seventh.java
@@ -0,0 +1,24 @@
+public class Seventh {
+    public class Solution {
+        public boolean checkValidString(String s) {
+            int leftMin = 0, leftMax = 0;
+            //Code Starts Here:
+            for (char c : s.toCharArray()) {
+                if (c == '(') {
+                    leftMin++;
+                    leftMax++;
+                } else if (c == ')') {
+                    leftMin--;
+                    leftMax--;
+                } else {
+                    leftMin--;
+                    leftMax++;
+                }
+                if (leftMax < 0) return false;
+                if (leftMin < 0) leftMin = 0;
+            }
+            
+            return leftMin == 0;
+        }
+    }
+}
