e7982f0832e50643e6fbeb4b49b387161f7dd391
sikand34 <sikand1365.be21@chitkara.edu.in>
2024-06-12 14:28:56 +0530

    Twelfth June Updated

---
+++ June/Twelfth.java
@@ -0,0 +1,28 @@
+public class Twelfth {
+    class Solution {
+        public void sortColors(int[] nums) {
+            int n = nums.length;
+            int low = 0;
+            int high = n-1;
+            int mid = 0;
+            while(mid<=high){
+                if(nums[mid] == 0){
+                    int temp = nums[low];
+                    nums[low] = nums[mid];
+                    nums[mid] = temp;
+                    mid++;
+                    low++;
+                }
+                else if(nums[mid] == 1){
+                    mid++;
+                }
+                else{
+                    int temp = nums[mid];
+                    nums[mid] = nums[high];
+                    nums[high] = temp;
+                    high--;
+                }
+            }
+        }
+    }
+}
